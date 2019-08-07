ace69247b16e4821e4802e3716b9cbb6201feb18
heliokamakawa <heliokamakawa@gmail.com>
2019-08-07 10:24:48 -0300

    'pri_ver'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'salvar', loadChildren: './estado/salvar/salvar.module#SalvarPageModule' },
+  { path: 'listar', loadChildren: './estado/listar/listar.module#ListarPageModule' },
 ];
 
 @NgModule({
